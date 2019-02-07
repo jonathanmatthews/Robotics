a5f88a26c4d050f2d7be56d210a47d0a1ea78143
David <dthomas.music207@yahoo.co.uk>
2019-02-07 20:20:45 +0000

    Created a robot class to control robot and read angles simultaneously

--- Code/change_stiffness.py
+++ Code/change_stiffness.py
@@ -1,21 +1,21 @@
 from naoqi import ALProxy
 
-def convert_stiff_to_numeric(stiff):
-    if stiff:
-        return 0.0, 'Stiffening'
-    else:
-        return 1.0, 'Loosening'
 
 def get_input():
-    stiff = input("Enter False to stiffen, True to un-stiffen \n")
-    if stiff not in [True, False]:
-        raise ValueError('Input not allowed')    
+    stiff = input("Stiffen or unstiffen? \n")    
     return stiff
 
-if __name__ == '__main__':
-    stiff = get_input()
-    stiff_numeric, confirm_text = convert_stiff_to_numeric(stiff)
-    print('{} up'.format(confirm_text))
+def change_stiffness(stiffness, part):
+    if stiffness in ['stiffen', 'Stiffen', 'stiff', 'Stiff']:
+        motion.setStiffnesses(part, 1.0)
+        return 'Stiffening'
+    else:
+        motion.setStiffnesses(part, 0.0)
+        return 'Loosening'
 
+if __name__ == '__main__':
     motion = ALProxy("ALMotion", "192.168.1.3", 9559)
-    motion.setStiffnesses("Body", stiff_numeric)
+
+    stiffness = get_input()
+    confirm_text = change_stiffness(stiffness, "Body")
+    print('{} up'.format(confirm_text))
