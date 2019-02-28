d42c6a53f02021e293c463c05655afd3fbfe9f3f
JamesDoering <jamesdoering13@googlemail.com>
2019-02-28 14:51:58 +0000

    Fixed the correct file this time

--- Code/algorithm_startup.py
+++ Code/algorithm_startup.py
@@ -6,18 +6,22 @@ class Start():
         self.duration = kwargs.get('duration', 10)
         self.wait_time = 1.3
         self.last_move = 0
-        self.set_posture('extended')
         pass
     
     def algo(self, values):
         print 'start', values['time']
         t = values['time']
-        if t > self.last_move + self.wait_time:
+        if t < 0.1:
             self.last_move = t
-            if self.position == 'extended':
-                self.set_posture('seated')
-            else:
-                self.set_posture('extended')
+            return 'extended'
+        
+        if t > 0.1:
+            if t > self.last_move + self.wait_time:
+                self.last_move = t
+                if values['pos'] == 'extended':
+                    return 'seated'
+                else:
+                    return 'extended'
 
         if time() - self.start_time > self.duration:
             return 'switch'
