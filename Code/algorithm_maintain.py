bb032f7a668a9a7853f16dbd0372f88348db0966
George <georgesheppard98@gmail.com>
2019-02-28 15:20:45 +0000

    remove touch

--- Code/algorithm_maintain.py
+++ Code/algorithm_maintain.py
@@ -2,12 +2,12 @@ from time import time
 
 class Maintain():
 
-    def __init__(self, values, **kwargs):
+    def __init__(self, values, all_data, **kwargs):
         self.start_time = time()
         self.maintain_angle = kwargs.get('maintain_angle', 10)
         self.duration = kwargs.get('duration', 15)
 
-    def algo(self, values):
+    def algo(self, values, all_data):
         print 'Maintain', values['time']
 
         if time() - self.start_time > self.duration:
