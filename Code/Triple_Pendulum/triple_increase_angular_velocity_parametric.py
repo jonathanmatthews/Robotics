213f6cd3f3b66e744f31c61c63671684dd6bae76
Jonathan Matthews <44363860+jonathanmatthews@users.noreply.github.com>
2019-03-20 14:24:16 +0000

    algorithm sort of works now (i think?)

--- Code/Triple_Pendulum/triple_increase_angular_velocity_parametric.py
+++ Code/Triple_Pendulum/triple_increase_angular_velocity_parametric.py
@@ -12,6 +12,7 @@ class TripleIncreaseAngularVelocity():
         self.previous_max_angle = total_angle(all_data['be'], all_data['se0'], all_data['se1']).max()
         self.previous_be = total_angle(values['be'], values['se0'], values['se1'])
         self.previous_av = values["av"]
+        self.already_passed_zero = False
     
 
     def algo(self, values, all_data):
@@ -22,37 +23,71 @@ class TripleIncreaseAngularVelocity():
         self.current_av = values["av"]
         print 'Time: {:.2f}'.format(values['time']), 'Total angle value: {:.2f}'.format(self.current_be)
         
-        if abs(self.previous_be) > abs(self.current_be): # Hit a maxima.
-	    if self.increasing:
-	        return ["lowered", 0.5] # This speed should be tested on the robot.
-	    
-	    else:
-	        return ["raised", 0.5]
-	
-	    self.previous_max_angle = self.previous_be
-	    print("max_angle", self.previous_be)
-	
-        elif abs(self.previous_av) > abs(self.current_av): # Hit a zero.
-	    if self.increasing:
-	        return ["raised", 0.5]
-	    
-	    else:
-	        return ["lowered", 0.5]
+        #if abs(self.current_av) <= abs(self.previous_av) and not self.already_passed_zero:
+        if sign_zero(self.current_av) != sign_zero(self.previous_av) and not self.already_passed_zero:
+            #print("AVs:", self.previous_av, self.current_av, "time:", values['time'], "raise")
+            self.previous_be = self.current_be
+            self.previous_av = self.current_av
+            self.already_passed_zero = True # Already past minima, don't send the signal again.
+
+            if self.increasing:
+                print "raise"
+                return ["raised", 0.5]
+            else:
+                return ["lowered", 0.5]
         
-        self.previous_be = self.current_be
-        self.previous_av = self.current_av
-        
+        # if(sign_zero(self.previous_be)==-1 and self.previous_be - self.current_be <0):
+        #     self.already_passed_minima = False # Moving back down.
+        #     self.previous_be = self.current_be
+        #     self.previous_av = self.current_av
+
+        #     self.previous_max_angle = self.previous_be
+        #     print('max_angle', self.previous_be)
+            
+        #     if(self.increasing == True):
+        #         return ['raised', 0.5]
+        #     elif(self.increasing == False):
+        #         return ['lowered', 0.5]
+            
+        # elif(sign_zero(self.previous_be)==1 and self.previous_be - self.current_be >0):
+        #     self.already_passed_minima = False # Moving back down.
+        #     self.previous_be = self.current_be
+        #     self.previous_av = self.current_av
+            
+        #     self.previous_max_angle = self.previous_be
+        #     print('max_angle', self.previous_be)
+            
+        #     if(self.increasing == True):
+        #         return ['raised', 0.5]
+        #     elif(self.increasing == False):
+        #         return ['lowered', 0.5]
+
+        if abs(self.previous_be) >= abs(self.current_be):
+            #print("BEs:", self.previous_be, self.current_be, "time", values['time'], "lower")
+            self.already_passed_zero = False # Moving back down.
+            self.previous_be = self.current_be
+            self.previous_av = self.current_av
+            
+            self.previous_max_angle = self.previous_be
+            print('max_angle', self.previous_be)
+            
+            if self.increasing:
+                print "lower"
+                return ['lowered', 0.5]
+            else:
+                return ['raised', 0.5]
+            
         # switch conditions
         if self.increasing:
             if abs(self.current_be) > self.max_angle:
                 return 'switch'
-	      
+
         else:
             print 'Decrease', values['time']
-            
+
             if abs(self.previous_max_angle) < self.min_angle:
                 return 'switch'
-	      
+
         if values['time'] - self.start_time > self.duration:
             return 'switch'
         
