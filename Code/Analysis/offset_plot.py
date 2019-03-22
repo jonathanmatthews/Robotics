aec312ca1fac4518c267f300df367547c345d992
George <georgesheppard98@gmail.com>
2019-03-22 20:26:53 +0000

    offset plot

--- Code/Analysis/offset_plot.py
+++ Code/Analysis/offset_plot.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 from graph_functions import *
 from sys import path
 path.insert(0, '..')
-from utility_functions import read_file, moving_average
+from utility_functions import read_file, moving_average, flatten
 import numpy as np
 from scipy.signal import find_peaks
+from scipy.optimize import curve_fit
 from collections import OrderedDict
 
 
@@ -14,11 +15,35 @@ offsets_names = np.genfromtxt('offset_data_collection.csv', delimiter=',', skip_
 
 files_offsets = {}
 for row in offsets_names:
-    files_offsets[row[0]] = list(row)[1:]
+    if row[0] != -0.1 and row[0] <= 0.05:
+        files_offsets[row[0]] = list(row)[1:]
 output_data_directory = '../Output_data/'
 
-for offset in files_offsets.keys():
-    dictionary_gradient = OrderedDict({})
+def linear_fit(x, m, c, d):
+    return m * x + c + d * x ** 2
+
+def exp_fit(x, b, m):
+    return b/m * (1 - np.exp(- b * x))
+
+
+# setup figure
+fig, ax = plt.subplots(
+    1, 1, figsize=(
+        13, 8))
+
+ax = format_graph(ax)
+
+
+information = OrderedDict({})
+
+for offset in sorted(files_offsets.keys()):
+    print 'Offset: {:.2f}'.format(offset)
+    information[offset] = {
+        'gradients': [],
+        'periods': [],
+        'gradients_error': [],
+    }
+
     for i, filename in enumerate(files_offsets[offset]):
         if filename != '':
             data = read_file(output_data_directory + filename)
@@ -26,40 +51,80 @@ for offset in files_offsets.keys():
             be = data['be']
             algorithm = data['algo']
 
-            # only positive big encoder times
-            time = time[be > 0]
-            be = be[be > 0]
+            for pos in range(2):
+                # find indexes where algorithm changes
+                algorithm_change_indexes = np.append(np.where(algorithm[:-1] != algorithm[1:])[0], np.array(len(algorithm) - 1))
 
-            # find indexes where algorithm changes
-            algorithm_change_indexes = np.append(np.where(algorithm[:-1] != algorithm[1:])[0], np.array(len(algorithm) - 1))
+                # only take data after has switched to quarter period
+                time = time[algorithm_change_indexes[1]:]
+                be = be[algorithm_change_indexes[1]:]
+                window = 21
 
-            # only take data after has switched to quarter period
-            time = time[algorithm_change_indexes[1]:]
-            be = be[algorithm_change_indexes[1]:]
+                be = np.array(moving_average(be, window_size=window))
+                time = time[(window-1)/2:-(window-1)/2]
 
-            # find peaks of big encoder
-            peak_index = find_peaks(be)[0]
-            maximas = be[peak_index]
-            max_times = time[peak_index]
+                if pos == 1:
+                    # only positive big encoder times
+                    time = time[be > 0]
+                    be = be[be > 0]
+                if pos == 0:
+                    time = time[be < 0]
+                    be = be[be < 0]
+                    be = np.abs(be)
 
-            # always ignore first two peaks after switching algorithm
-            maximas = maximas[2:]
-            max_times = max_times[2:]
+                # find peaks of big encoder
+                peak_index = find_peaks(be)[0]
+                maximas = be[peak_index]
+                max_times = time[peak_index]
 
-            # plt.plot(max_times, maximas, label='Offset: {}s, run {}'.format(offset, i+1))
-            gradient = np.diff(maximas)
-            angles = maximas[:-1]
+                # always ignore first two peaks after switching algorithm
+                maximas = maximas[2:]
+                max_times = max_times[2:]
 
-            # find gradient at each value and round it to nearest 0.5, then average 
-            # all results in that bin
-            for value, gradient_ in zip(angles, gradient):
-                round_value = round(2* value)/2
-                if round_value not in dictionary_gradient.keys():
-                    dictionary_gradient[round_value] = []
-                dictionary_gradient[round_value].append(gradient_) 
-    # only plot after each offset has been completed
-    avg_grad = [np.mean(dictionary_gradient[key]) for key in dictionary_gradient.keys()]
-    plt.plot(dictionary_gradient.keys(), avg_grad, label='Offset: {}s, run {}'.format(offset, i+1))
+                # filter out any be encoder recording errors
+                no_large_change_indexes = np.diff(maximas) < 0.25
+                max_times = max_times[:-1][no_large_change_indexes]
+                maximas = maximas[:-1][no_large_change_indexes]
+
+                plt.plot(max_times, maximas, label=offset)
+
+                # curve fit doesn't work with different data types
+                max_times = max_times.astype(dtype=np.float32)
+                maximas = maximas.astype(dtype=np.float32)
+                errors = np.array([0.08]*len(maximas))
+                errors = errors.astype(dtype=np.float32)
+
+                periods = np.diff(max_times)
+                information[offset]['periods'] = list(information[offset]['periods']) + list(periods)
+
+                # popt, pcov = curve_fit(linear_fit, max_times, maximas, p0=[0.1, 4.0, -0.01], sigma=errors)
+                popt, pcov = curve_fit(exp_fit, max_times, maximas, p0=[0.01, 0.001], sigma=errors)
+                print 'Gradient: {}, Intercept: {}'.format(*popt)
+
+                fitted_times = np.linspace(min(max_times), max(max_times), 50)
+                # fitted_maximas = linear_fit(fitted_times, *popt)
+                fitted_maximas = exp_fit(fitted_times, *popt)
+                plt.plot(fitted_times, fitted_maximas, label='Fit')
+                plt.legend(loc='best')
+                plt.show()
+                perr = np.sqrt(np.diag(pcov))
+
+                information[offset]['gradients'].append(popt[0])
+                information[offset]['gradients_error'].append(perr[0])
+
+
+
+average_gradients = [np.mean(information[key]['gradients']) for key in information.keys()]
+std_gradients = [np.std(information[key]['gradients']) for key in information.keys()]
+average_periods = [np.mean(information[key]['periods']) for key in information.keys()]
+std_periods = [np.std(information[key]['periods']) for key in information.keys()]
+increase_per_cycle = [grad * period for grad, period in zip(average_gradients, average_periods)]
+std_increase_per_cycle = [np.sqrt((dg/p)**2 + (-g*dp/p**2)**2) for g, p, dg, dp in zip(average_gradients, average_periods, std_gradients, std_periods)]
+
+plt.errorbar(information.keys(), increase_per_cycle, yerr=std_increase_per_cycle, label='Increase per cycle', fmt='o')
+plt.xlabel('Offset (s)')
+plt.ylabel('Increase per cycle ' + r'$(^o)$')
+plt.title('Comparison between different offsets\nand the rate of increase of angle')
 
 plt.legend(loc='best')
 plt.show()
