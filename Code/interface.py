748a6cc594fe0f8138eddf3d72082c83986ded00
demo <demo@y3labpc60.ph.bham.ac.uk>
2019-03-26 14:03:34 +0000

    James' data

--- Code/interface.py
+++ Code/interface.py
@@ -411,7 +411,7 @@ class Interface(Algorithm):
 if __name__ == '__main__':
     # Raising error after loosening as then script that plots
     # afterwards doesn't bother
-    interface = Interface(setup, period=0.02)
+    interface = Interface(setup, period=0.01)
     try:
         interface.run(filename='Accelerometer Algorithm')
     except KeyboardInterrupt:
