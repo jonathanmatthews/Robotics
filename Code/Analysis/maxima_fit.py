b3f599ce0f9c7215fa33a0b3715bf33af81c8ca8
Jonathan Matthews <44363860+jonathanmatthews@users.noreply.github.com>
2019-03-29 04:08:12 +0000

    fit done in a single file

--- Code/Analysis/maxima_fit.py
+++ Code/Analysis/maxima_fit.py
@@ -59,8 +59,17 @@ def plot_rot():
     fitted_rot = fit_rotational(n_rot[:101], *rot_params[0])
     eqn_rot = "{} n + {}".format(round(rot_params[0][0], 3), round(rot_params[0][1], 3))
 
+    fig, ax = plt.subplots(
+    1, 1, figsize=(
+        8, 6))
+    ax.set_facecolor("#eeeeee")
+
     plt.plot(n_rot[:101], fitted_rot, Label="Rotational (Quarter Period)")
     plt.plot(n_rot, peaks_rot, 'g.', label=r"Linear fit, $\theta$ = " + eqn_rot)
+    plt.xlabel("Cycle number")
+    plt.ylabel(r"Amplitude ($^o$)")
+    plt.title("Fit of rotational amplitude gain")
+    #plt.set_facecolor('#eeeeee')
     plt.legend()
     plt.show()
 
@@ -73,17 +82,58 @@ def plot_para():
     para_params = curve_fit(fit_parametric_2, n_para[35:250], peaks_para[35:250], p0=[1/86.5, 1, 0])
     print para_params[0]
     fitted_para = fit_parametric_2(n_para[35:250], *para_params[0])
-    eqn_para = "{} exp({} n) + {}".format(*(round(i, 3) for i in  para_params[0]))
+    eqn_para = "{} exp({} n) + {}".format(round(para_params[0][1], 3), round(para_params[0][0], 3), round(para_params[0][2], 3))
+    #eqn_para = ""
+
+    fig, ax = plt.subplots(
+    1, 1, figsize=(
+        8, 6))
+    ax.set_facecolor("#eeeeee")
+
+    plt.plot(n_para, peaks_para, "g.", Label="Parametric (Quarter Period)")
+    plt.plot(n_para[35:250], fitted_para, label=r"exponential fit, $\theta$ = " + eqn_para)
+    plt.xlabel("Cycle number")
+    plt.ylabel(r"Amplitude ($^o$)")
+    plt.title("Fit of parametric amplitude gain")
+    plt.legend()
+    plt.show()
+
+def plot_both():
+    # Get rotational.
+    rot_params = curve_fit(fit_rotational, n_rot[:101], peaks_rot[:101])
+    # Linear region seems to end at ~100 cycles.
+    fitted_rot = fit_rotational(n_rot[:101], *rot_params[0])
+    eqn_rot = "{} n + {}".format(round(rot_params[0][0], 3), round(rot_params[0][1], 3))
+
+    fig, ax = plt.subplots(
+    1, 1, figsize=(
+        8, 6))
+    ax.set_facecolor("#eeeeee")
+
+    plt.plot(n_rot, peaks_rot, 'r.', Label="Rotational (Quarter Period)")
+    plt.plot(n_rot[:101], fitted_rot, label=r"Linear fit, $\theta$ = " + eqn_rot)
+    plt.xlabel("Cycle number")
+    plt.ylabel(r"Amplitude ($^o$)")
+    #plt.set_facecolor('#eeeeee')
+
+
+    # Get parametric.
+    para_params = curve_fit(fit_parametric_2, n_para[35:250], peaks_para[35:250], p0=[1/86.5, 1, 0])
+    print para_params[0]
+    fitted_para = fit_parametric_2(n_para[35:250], *para_params[0])
+    eqn_para = "{} exp({} n) + {}".format(round(para_params[0][1], 3), round(para_params[0][0], 3), round(para_params[0][2], 3))
     #eqn_para = ""
 
     plt.plot(n_para, peaks_para, "g.", Label="Parametric (Quarter Period)")
     plt.plot(n_para[35:250], fitted_para, label=r"exponential fit, $\theta$ = " + eqn_para)
+    plt.xlabel("Cycle number")
+    plt.ylabel(r"Amplitude ($^o$)")
+    plt.title("Fit of per-cycle amplitude gain")
     plt.legend()
     plt.show()
 
 
-plot_rot()
-plot_para()
-
-
+#plot_rot()
+#plot_para()
+plot_both()
 
