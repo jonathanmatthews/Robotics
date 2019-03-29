f48f0186d5f7df162b866566f836e8f57af5a0d1
Jonathan Matthews <44363860+jonathanmatthews@users.noreply.github.com>
2019-03-29 12:02:01 +0000

    Edited fit

--- Code/Analysis/maxima_fit_2.py
+++ Code/Analysis/maxima_fit_2.py
@@ -11,31 +11,38 @@ from graph_functions import format_graph
 #parametric = read_file("../Output_data/Parametric No Masses 800secs")['be']
 #rotational = read_file("../Output_data/Rotational No Masses 400secs")['be']
 
-parametric = read_file("../Output_data/26-03-2019 18:17:02 Para 600 secs Org")['be']
-rotational = read_file("../Output_data/26-03-2019 18:30:45 Rot 600 secs Org")['be']
+parametric = read_file("../Output_data/26-03-2019 18:17:02 Para 600 secs Org")
+rotational = read_file("../Output_data/26-03-2019 18:30:45 Rot 600 secs Org")
 
-para_dt = np.mean(np.diff(read_file("../Output_data/26-03-2019 18:17:02 Para 600 secs Org")['time']))
-rot_dt = np.mean(np.diff(read_file("../Output_data/26-03-2019 18:30:45 Rot 600 secs Org")['time']))
+para_dt = np.mean(np.diff(parametric['time']))
+rot_dt = np.mean(np.diff(rotational['time']))
+
+index_para = find_peaks(parametric['be'])[0]
+index_rot = find_peaks(rotational['be'])[0]
+
+peaks_para = abs(parametric['be'][index_para])
+peaks_rot = abs(rotational['be'][index_rot])
+
+times_para = parametric['time'][index_para]
+times_rot = abs(rotational['time'][index_rot])
 
 
-
-
-index_para = find_peaks(parametric)[0]
-index_rot = find_peaks(rotational)[0]
-
-peaks_para = abs(parametric[index_para])
-peaks_rot = abs(rotational[index_rot])
 
 ##### George's magical filter.
 be = peaks_para
+t = times_para
 no_large_change_indexes = np.diff(be) < 0.25
-#t = t[:-1][no_large_change_indexes]
+t = t[:-1][no_large_change_indexes]
 be = be[:-1][no_large_change_indexes]
 peaks_para = be
+times_para = t
 #####
 
-n_para = np.arange(len(peaks_para))*para_dt/2.55
-n_rot = np.arange(len(peaks_rot))*para_dt/2.55
+n_para = times_para
+n_rot = times_rot
+
+t_para = np.arange(len(peaks_para))*para_dt/2.55
+t_rot = np.arange(len(peaks_rot))*rot_dt/2.55
 
 def fit_rotational(n, m, c):
     """
@@ -55,6 +62,8 @@ def fit_parametric(n, m, c, q, k):
     c : y-intercept of linear,
     q : cycle number at which to change to exponential,
     k : exponent factor.
+
+    Don't bother with this one, this is just here incase I need it in the future.
     """
     # Linear then exponential.
     # y(n<q) = mn+c
@@ -71,6 +80,7 @@ def fit_parametric_2(n, k, a, c):
 def plot_rot():
     """
     Fit linear region of rotational, then plot.
+    This function only exists for testing values on rotational.
     """
     rot_params = curve_fit(fit_rotational, n_rot[20:61], peaks_rot[20:61])
     # Linear region seems to end at ~100 cycles.
@@ -94,10 +104,11 @@ def plot_rot():
 def plot_para():
     """
     Fit linear/exponential region of parametric, then plot.
+    This function exists only for testing values on parametric.
     """
     #print [len(n_para), len(peaks_para)]
 
-    para_params = curve_fit(fit_parametric_2, n_para[35:250], peaks_para[35:250], p0=[1/86.5, 1, 0])
+    para_params = curve_fit(fit_parametric_2, n_para[35:250], peaks_para[35:250], p0=[0.0064, 1, 0])
     print para_params[0]
     fitted_para = fit_parametric_2(n_para[35:250], *para_params[0])
     eqn_para = "{} exp({} n) + {}".format(round(para_params[0][1], 3), round(para_params[0][0], 3), round(para_params[0][2], 3))
@@ -120,7 +131,7 @@ def plot_both():
     # Get rotational.
     rot_params = curve_fit(fit_rotational, n_rot[20:61], peaks_rot[20:61])
     # Linear region seems to end at ~100 cycles.
-    fitted_rot = fit_rotational(n_rot[:101], *rot_params[0])
+    fitted_rot = fit_rotational(n_rot[20:61], *rot_params[0])
     eqn_rot = "{} n + {}".format(round(rot_params[0][0], 3), round(rot_params[0][1], 3))
 
     fig, ax = plt.subplots(
@@ -128,8 +139,8 @@ def plot_both():
         8, 6))
     ax.set_facecolor("#eeeeee")
 
-    plt.plot(n_rot, peaks_rot, 'r.', Label="Rotational (Quarter Period)")
-    plt.plot(n_rot[20:61], fitted_rot, label=r"Linear fit, $\theta$ = " + eqn_rot)
+    plt.plot(n_rot, peaks_rot, '.', color="#00004d", Label="Rotational (Quarter Period)")
+    plt.plot(n_rot[20:61], fitted_rot, color="#456DBA", label=r"Linear fit, $\theta$ = " + eqn_rot)
     plt.xlabel("Cycle number")
     plt.ylabel(r"Amplitude ($^o$)")
     #plt.set_facecolor('#eeeeee')
@@ -137,14 +148,16 @@ def plot_both():
     ax = format_graph(ax)
 
     # Get parametric.
-    para_params = curve_fit(fit_parametric_2, n_para[35:250], peaks_para[35:250], p0=[0.0001, 1, 0])
+    para_params = curve_fit(fit_parametric_2, n_para[20:90], peaks_para[20:90], p0=[np.log(4)/26, 1, 0])
     print para_params[0]
-    fitted_para = fit_parametric_2(n_para[35:250], *para_params[0])
-    eqn_para = "{} exp({} n) + {}".format(round(para_params[0][1], 3), round(para_params[0][0], 3), round(para_params[0][2], 3))
+    fitted_para = fit_parametric_2(n_para[20:90], *para_params[0])
+    eqn_para = "{} exp({} n) + {}".format(round(para_params[0][1], 3), round(para_params[0][0], 5), round(para_params[0][2], 3))
     #eqn_para = ""
+    print "parametric errors: {}".format(np.sqrt(np.diag(para_params[1])))
+    print "rotational errors: {}".format(np.sqrt(np.diag(rot_params[1])))
 
-    plt.plot(n_para, peaks_para, "g.", Label="Parametric (Quarter Period)")
-    plt.plot(n_para[35:250], fitted_para, label=r"exponential fit, $\theta$ = " + eqn_para)
+    plt.plot(n_para, peaks_para, ".", color="#CC0000", Label="Parametric (Quarter Period)")
+    plt.plot(n_para[20:90], fitted_para, color="#F9A628", label=r"exponential fit, $\theta$ = " + eqn_para)
     plt.xlabel("Period number")
     plt.ylabel(r"Amplitude ($^o$)")
     plt.title("Fit of per-period amplitude gain")
@@ -157,7 +170,7 @@ def plot_both():
 plot_both()
 
 
-#plt.plot(n_para, peaks_para)
+#plt.plot(t_para, peaks_para)
 #plt.show()
 
 
